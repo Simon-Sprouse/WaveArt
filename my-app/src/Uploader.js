9b4f389ce2da923ea28cfb6a13f4353c3eefe5ca
Simon Sprouse <simonhsprouse@gmail.com>
2024-10-03 23:13:07 -0500

    conversion to grayscale

--- my-app/src/Uploader.js
+++ my-app/src/Uploader.js
@@ -10,7 +10,32 @@ function Uploader() {
         if (file) { 
             const reader = new FileReader();
             reader.onload = () => { 
-                setImage(reader.result);
+                const img = new Image();
+                img.src = reader.result;
+                img.onload = () => { 
+                    const canvas = document.createElement("canvas");
+                    const ctx = canvas.getContext("2d");
+                    canvas.width = img.width;
+                    canvas.height = img.height;
+
+                    ctx.drawImage(img, 0, 0);
+
+                    const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+                    const data = imageData.data;
+
+                    for (let i = 0; i < data.length; i += 4) { 
+                        const avg = Math.floor((data[i] + data[i+1] + data[i+2]) / 3);
+                        data[i] = avg;
+                        data[i + 1] = avg;
+                        data[i + 2] = avg;
+                    }
+
+                    ctx.putImageData(imageData, 0, 0);
+                    const grayScaleImage = canvas.toDataURL();
+                    setImage(grayScaleImage);
+
+
+                }
             };
             reader.readAsDataURL(file);
         }
